100674830450672bb26ed87c6495a748c3543fd7
daquinonesr <47469291+daquinonesr@users.noreply.github.com>
2019-04-06 14:47:27 -0600

    Add files via upload

--- Lab5.py
+++ Lab5.py
@@ -12,7 +12,7 @@ import time
 
 class BST(object):
     # Constructor
-    def __init__(self, item, left=None, right=None):  
+    def __init__(self, item=[], left=None, right=None):  
         self.item = item
         self.left = left 
         self.right = right      
@@ -28,6 +28,7 @@ class HashTableC(object):
         self.item = []
         for i in range(size):
             self.item.append([])
+        self.num_items=0
         
 def InsertC(H,k,l):
     # Inserts k in appropriate bucket (list) 
@@ -67,7 +68,7 @@ def Similarity(H,file):
         word[1]=word[1].replace('\n',' ')
         a=FindC(H,word[0])
         b=FindC(H,word[1])
-        print('Similarity', word,'=',round(np.sum(a*b)/(math.sqrt(np.sum(a*a)))*math.sqrt(np.sum(b*b))))
+        print('Similarity', word,'=',round(np.sum(a[2]*b[2]))/((math.sqrt(np.sum(a[2]*a[2])))*(math.sqrt(np.sum(b[2]*b[2])))))
 
 def HashBuilder(v):
     print('Building hash table with chaining',end=' ')
@@ -78,10 +79,12 @@ def HashBuilder(v):
         letters=line.split(" ")
         word=letters[0]
         embed = np.empty([50], dtype=float)
-        for j in range(len(letters)):
-            embed[j]=letters[j]
+        counter=0
+        for j in range(1,len(letters)):
+            embed[counter]=letters[j]
+            counter+=1
         InsertC(H1,word,embed)
-        H1.num_items=H1.num_items+1
+        H1.num_items+=1
     EmptyLists=((EmptyPer(H1))/len(H1.item) )*100
     deviation=SDev(H1)
     print('Final Table Size: ',len(H1.item))
@@ -102,7 +105,7 @@ def ExpandHash(H):
     
 
 def loadFactor(H):
-    return H.num_items//len(H.items)
+    return H.num_items//len(H.item)
 
 
 def SDev(H):
@@ -110,50 +113,47 @@ def SDev(H):
     k=loadFactor(H)
     for i in H.item:
         a=a+ len(i)-k
-    standard=((1/len(H.items)*a)/(len(H.items)))*100
+    standard=((1/len(H.item)*a)/(len(H.item)))*100
     return standard
     
 #All of this code belong to BST
 
 def Tree(a):
     print('Building binary search tree')
-    k=None
     nodeCounter=0
+    tree=None
     for i in a:
         line=i
         text=line.split(" ")
         word = text[0]
         j= value(word)
+        count=0
         e=np.empty([50],dtype=float)
         for k in range(1,len(text)):
-            e[k]=text[k]
-        k=Insert(k,[j,word,e])
+            e[count]=text[k]
+            count+=1
+        tree=Insert(tree,[j,word,e])
         nodeCounter+=1
     print('Number of nodes: ',nodeCounter)
-    return k
+    return tree
+
 
 def Insert(T,newItem):
     if T==None:
         T=BST(newItem)
     elif T.item[0]>newItem[0]:
         T.left=Insert(T.left,newItem)
-    else:
+    elif T.item[0]<newItem[0]:
         T.right=Insert(T.right,newItem)
     return T
 
 def height(T):
     counter=0
     temp1=T
-    temp2=T
     while temp1 is not None:
         counter=counter+1
         temp1=temp1.left
     counter=0
-    while temp2 is not None:
-        counter=counter+1
-        temp2=temp2.right
-    if temp1>temp2:
-        return temp2
     return temp1
 
 def value(w):
@@ -164,27 +164,27 @@ def value(w):
     return counter
 
 def wordfinder(T,k):
-    temp=T
-    while temp is not None:
-        if temp.item[0]==value(k) and temp.item[1]==k:
-            return temp.item
-        elif temp.item[0]<value(k):
-            temp=temp.right
+    while T is not None:
+        if T.item[0]==value(k) or T.item[1]==k:
+            return T.item
+        elif T.item[0]<value(k):
+            T=T.right
         else:
-            temp=temp.left
+            T=T.left
     return None
 
 def similaritybst(T,file):
+    temp=T
     print('Reading word file to determine similarities')
     print()
     print('Word similarities found: ')
     print()
     for words in file:
         words=words.split(',')
-        words[1]=words[1].replace('\n',' ').replace()
-        words2=wordfinder(T,words[0])
-        words3=wordfinder(T,words[1])
-        print('Similarity', words,'=',round(np.sum(words2*words3)/(math.sqrt(np.sum(words2*words2)))*math.sqrt(np.sum(words3*words3))))
+        words[1]=words[1].replace('\n',' ')
+        b=wordfinder(temp,words[1])
+        a=wordfinder(temp,words[0])
+        print('Similarity', words,'=',round(np.sum(a[2]*b[2]))/((math.sqrt(np.sum(a[2]*a[2])))*(math.sqrt(np.sum(b[2]*b[2])))))
     
     
 #main method
@@ -193,15 +193,20 @@ file=open("glove.6B.50d.txt","r")
 words=open("words.txt","r")
 type(userinput)
 if userinput=='2':
+    start = time. time()
     print('Choice: ',2)
     HT=HashBuilder(file)
     Similarity(HT,words)
+    end = time. time()
+    print(end - start)
     
 if userinput=='1':
+    start = time. time()
     print('Choice: ',1)
     BS=Tree(file)
     similaritybst(BS,words)
-    
+    end = time. time()
+    print(end - start)
     
 
     
